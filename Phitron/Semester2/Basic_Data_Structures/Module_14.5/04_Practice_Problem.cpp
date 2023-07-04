851f119d63981aee8dc84047330e35ac2205a2ed
Masud Abdullah <abdullahtamim885@gmail.com>
2023-07-04 23:49:31 +0600

    Practice Problem 04

---
+++ Phitron/Semester2/Basic_Data_Structures/Module_14.5/04_Practice_Problem.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    queue<int>Q1;
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        int x;
+        cin>>x;
+        Q1.push(x);
+    }
+    stack<int>st;
+    while(!Q1.empty())
+    {
+        st.push(Q1.front());
+        Q1.pop();
+    }
+    queue<int>Q2;
+    while(!st.empty())
+    {
+        Q2.push(st.top());
+        st.pop();
+    }
+
+    while(!Q2.empty())
+    {
+        cout<<Q2.front()<<" ";
+        Q2.pop();
+    }
+    cout<<endl;
+
+    return 0;
+}
