4e4f3fed081978c1d1b3ab299b144e72b7bbeff9
Masud Abdullah <abdullahtamim885@gmail.com>
2023-08-18 01:45:01 +0600

    Shortest Distance Between Two Nodes in a Graph

---
+++ Phitron/Semester2/Introduction to Algorithms/Module 09/Shortest_Distance.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<vector<int>> adj(N);
+bool visited[N];
+int level[N];
+
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+
+    visited[s] = true;
+    
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (auto v : adj[u])
+        {
+            if (!visited[v])
+            {
+                q.push(v);
+                visited[v] = true;
+                level[v] = level[u] + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int s, d;
+        cin >> s >> d;
+
+        memset(level, 0, sizeof(level));
+        memset(visited, false, sizeof(visited));
+
+        bfs(s);
+        cout << level[d] << endl;
+    }
+    return 0;
+}
