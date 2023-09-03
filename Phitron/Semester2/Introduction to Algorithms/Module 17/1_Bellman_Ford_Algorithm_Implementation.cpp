20f181ef661bf4bdb7fb8709ff13d3f985235be7
Masud Abdullah <abdullahtamim885@gmail.com>
2023-09-04 01:51:40 +0600

    Bellman Ford Algorithm Implementation

---
+++ Phitron/Semester2/Introduction to Algorithms/Module 17/1_Bellman_Ford_Algorithm_Implementation.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Edge
+{
+public:
+    int u;
+    int v;
+    int w;
+    Edge(int u, int v, int w)
+    {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+};
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> ans;
+    for (int i = 1; i <= e; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        Edge ed(u, v, w);
+        ans.push_back(ed);
+        // cout << ed.u << " " << ed.v << " " << ed.w << endl;
+    }
+
+    int dist[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        dist[i] = INT_MAX;
+    }
+    dist[1] = 0; // Source holo 1. Tai source er distance 0 kore deua hoise //
+
+    for (int i = 1; i <= n - 1; i++) // koto bar (n-1) relaxation hobe sheitar jonno ei loop //
+    {
+        for (int j = 0; j < ans.size(); j++)
+        {
+            Edge ed = ans[j];
+            int u = ed.u;
+            int v = ed.v;
+            int w = ed.w;
+
+            if (dist[u] + w < dist[v])
+            {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Node " << i << " : " << dist[i] << endl;
+    }
+    return 0;
+}
