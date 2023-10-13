87fd461d5e0106173b3087b293ad4e101e68eb95
Masud Abdullah <abdullahtamim885@gmail.com>
2023-10-14 02:18:44 +0600

    Topic Wise

--- Phitron/Semester3/XPSC_Phitron/Class - 01 (Sliding Window 1)/A.cpp
+++ Phitron/Semester3/XPSC_Phitron/Class - 01 (Sliding Window 1)/A.cpp
@@ -47,7 +47,80 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
 //problem 02: First negative integer in every window of size k
+///****************************Nijer niyome***********************************
 
+//{ Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<long long> printFirstNegativeInteger(long long int arr[],
+//                                              long long int n, long long int k);
+
+// // Driver program to test above functions
+// int main() {
+//     long long int t, i;
+//     cin >> t;
+//     while (t--) {
+//         long long int n;
+//         cin >> n;
+//         long long int arr[n];
+//         for (i = 0; i < n; i++) {
+//             cin >> arr[i];
+//         }
+//         long long int k;
+//         cin >> k;
+
+//         vector<long long> ans = printFirstNegativeInteger(arr, n, k);
+//         for (auto it : ans) cout << it << " ";
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+// // } Driver Code Ends
+
+
+// vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) 
+// {
+//     vector<long long>ng,ans;
+//     bool f=true;
+//     for(int i=0;i<N;i++)
+//     {
+//         if(A[i]<0)
+//         {
+//             ng.push_back(i);
+//             f=false;
+//         }
+//     }
+//     int it=0;
+
+//     for(int i=0;i<N-K+1;i++)
+//     {
+//         if(f)ans.push_back(0);
+//         else
+//         {
+//             if(ng[it]>=i && ng[it]<i+K)ans.push_back(A[ng[it]]);
+//             else if(ng[it]<i)
+//             {
+//                 if(it<ng.size()-1)it++;
+            
+//                 if(ng[it]>=i && ng[it]<i+K)ans.push_back(A[ng[it]]);
+//                 else ans.push_back(0);
+//             }
+//             else ans.push_back(0);
+//         }
+//     }
+
+//     // for(auto val:ans)
+//     // {
+//     //     cout<<val<<" ";
+//     // }
+//     // cout<<'\n';
+//     return ans;
+// }
+
+
+//*************************************Pathan Vai er Niyome***************************
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -78,42 +151,26 @@ int main() {
 
 // } Driver Code Ends
 
-
 vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int K) 
 {
-    vector<long long>ng,ans;
-    bool f=true;
-    for(int i=0;i<N;i++)
+    long long i=0,j=0;
+    queue<long long>q;
+    vector<long long>ans;
+    while(j<N)
     {
-        if(A[i]<0)
+        if(A[j]<0)q.push(A[j]);
+        if(j>=K-1)
         {
-            ng.push_back(i);
-            f=false;
-        }
-    }
-    int it=0;
-
-    for(int i=0;i<N-K+1;i++)
-    {
-        if(f)ans.push_back(0);
-        else
-        {
-            if(ng[it]>=i && ng[it]<i+K)ans.push_back(A[ng[it]]);
-            else if(ng[it]<i)
-            {
-                if(it<ng.size()-1)it++;
-            
-                if(ng[it]>=i && ng[it]<i+K)ans.push_back(A[ng[it]]);
-                else ans.push_back(0);
-            }
+            if(!q.empty())ans.push_back(q.front());
             else ans.push_back(0);
+            
+            if(A[i]<0)q.pop();
+            i++;
         }
+        j++;
     }
-
-    // for(auto val:ans)
-    // {
-    //     cout<<val<<" ";
-    // }
-    // cout<<'\n';
     return ans;
 }
+
+//Problem:3
+
